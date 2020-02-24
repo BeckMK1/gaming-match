48dd4767ee3f652f08a58b52c8e09dfb8f2550d0
Mikkel Beck Knudsen <mikkel1a2@gmail.com>
2020-02-24 11:10:54 +0100

    make post almost done

--- js/makePost.js
+++ js/makePost.js
@@ -3,13 +3,18 @@
 // inporting game title 
 function appendgamesforpost(games){
     let htmlTemplate="";
+    let htmlTemplate2="";
     for (let game of games){
       console.log(games)
   
   htmlTemplate +=`
   <option value="${game.title}">${game.title}</option>
-  ` 
+  `;
+  htmlTemplate2 +=
+  `<option value="${game.maxPlayerCount}">${game.maxPlayerCount}</option>
+  `;
   document.querySelector("#gameSelection").innerHTML = htmlTemplate;
+  document.querySelector("#playercount").innerHTML= htmlTemplate2;
   
     }
   
@@ -48,6 +53,7 @@ const _postRef = db.collection("posts");
 function addPost(){
 _postRef.add({
     title: document.querySelector("#gameSelection").value,
-    palyercount: document.querySelector("#numberOfPlayers").value,
+    palyercount: document.querySelector("#playercount").value,
+    tag: document.querySelector("#tags").value,
 })
 }
