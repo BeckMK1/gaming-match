a422fa693e86ab4c210ac48e788fdb6697f7d659
Mikkel Beck Knudsen <mikkel1a2@gmail.com>
2020-02-26 10:59:09 +0100

    fixed front page

--- js/main.js
+++ js/main.js
@@ -31,14 +31,15 @@ function signIn(){
         var errorMessage = error.message;
         // ...
       });
-      navigateTo("profilkasse")
+      navigateTo("profile")
     }
     firebase.auth().onAuthStateChanged((user) => {
         if (user) {
-          // User logged in already or has just logged in.
+        document.querySelector('#userogdrop').style.display = "block";
           console.log(user.uid);
         } else {
           // User not logged in or has just logged out.
+
         }
       });
 
