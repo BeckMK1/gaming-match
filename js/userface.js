5f3159c7c4e63896dd7843962fe9d3b860166d84
Nikolaj Egeskov Nielsen <Nikolajegeskov@hotmail.com>
2020-02-26 14:43:14 +0100

    update

--- js/userface.js
+++ js/userface.js
@@ -1,6 +1,6 @@
 const _usersref = db.collection("users");
 function appendUserForUserface(){
-let currentUser =firebase.auth().currentUser 
+let currentUser =firebase.auth().currentUser
 _usersref.doc(currentUser.uid).onSnapshot(function (userData){
 console.log(userData.data())
 // console.log(currentUser)
@@ -8,33 +8,12 @@ document.querySelector("#userface").innerHTML=`
 <img src="${userData.data().img}">
 <p>${userData.data().displayName}</p>
 `
-})
-}
-const _usersref1 = db.collection("users");
-let userface1=[];
-_usersref1.onSnapshot(function(snapshotData) {
-snapshotData.forEach(function(doc){
-  let _usersref1 = doc.data();
-
-  _usersref.uid = doc.uid;
-  userface.push(_usersref1);
-
-});
-console.log(_usersref1);
-appenduserimg(_usersref1);
-
-});
-function appenduserimg(_usersref1){
-let htmlTemplate="";
-for (let _usersref1 of userface1){
-  console.log(_usersref1)
-
-htmlTemplate =`<img src="${_usersref1.img}">
-
+document.querySelector("#userface1").innerHTML=`
+<img src="${userData.data().img}">
 `
+document.querySelector(".userinfo").innerHTML=`
 
-document.querySelector("#profileimages").innerHTML = htmlTemplate;
-
-}
-
+<p>username:<br>${userData.data().displayName}</p>
+`
+})
 }
