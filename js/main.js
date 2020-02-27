0144c1bd343aeff1439934bf0d754f252ccb72b1
henrikviera <60606263+henrikviera@users.noreply.github.com>
2020-02-27 10:26:09 +0100

    opdate

--- js/main.js
+++ js/main.js
@@ -90,6 +90,12 @@ function logout() {
   firebase.auth().signOut();
   console.log("logout");
 }
+
+function goBack() {
+  window.history.back();
+}
+
+
 // function appendUserFace(user){
 // document.querySelector("#userface").innerHTML+=`
 // <p>${user.displayName}</p>
